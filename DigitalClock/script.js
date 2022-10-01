b99caebbd705e440ca22086bfe3181a3d34b2d93
BuiltinCoders <73709535+BuiltinCoders@users.noreply.github.com>
2022-10-02 00:51:56 +0530

    Update script.js

--- DigitalClock/script.js
+++ DigitalClock/script.js
@@ -1,4 +1,4 @@
-function now() {
+function clock() {
     let dayArr = ['Mon', 'Tue', 'Wed', 'Thus', 'Fri', 'Sat', 'Sun']
     let monthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     let time = new Date();
@@ -6,22 +6,11 @@ function now() {
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
-    let date = time.getDate();
-    let day = time.getDay();
-    let setmonth = time.setMonth(9);
-    let month = time.getMonth();
-    let year = time.getFullYear();
-
 
 
     let hourSection = document.getElementById('hour').innerHTML = hour;
     let minSection = document.getElementById('min').innerHTML = min;
     let secSection = document.getElementById('sec').innerHTML = sec;
-    let dateSection = document.getElementById('date').innerHTML = date;
-    let dayText = document.getElementById('day-value').innerHTML = dayArr[day-1];
-    let monthSection = document.getElementById('month').innerHTML = month+1;
-    let monthText = document.getElementById('month-value').innerHTML = monthArr[month + 1];
-    let yearection = document.getElementById('year').innerHTML = year;
 }
 
-setInterval(now, 1000);
+setInterval(clock, 1000);
