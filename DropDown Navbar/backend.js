9c89c6d0ff4b75a9afc6f6d748a8280ab632ba19
BuiltinCoders <73709535+BuiltinCoders@users.noreply.github.com>
2022-10-02 18:03:01 +0530

    added event listener

--- DropDown Navbar/backend.js
+++ DropDown Navbar/backend.js
@@ -1,3 +1,17 @@
+// --------------------------- Account Button -----------------------
+let accountLogo = document.getElementById("account-button");
+let accountItem = document.getElementById("account-items");
+
+accountLogo.addEventListener("click", function toggleHide() {
+    if (accountItem.style.display == "flex") {
+        accountItem.style.display = "none";
+        accountLogo.style.borderColor = "white";
+    } else {
+        accountItem.style.display = "flex";
+        accountLogo.style.borderColor = "rgb(0, 131, 218)";
+    }
+});
+
 // ----------------------------- product item --------------------------
 let product = document.getElementById("products");
 let productItem = document.getElementById("product-items");
